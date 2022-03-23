7112dae46556e7743692a8173709876749acc620
Rallijuoma <huuskonen39@gmail.com>
2022-03-23 12:15:59 +0200

    Part 1.2

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  const title = 'Superadvanced web phonebook app'
+  const name1 = 'John Doe'
+  const phone1 = '358401234567'
+  const name2 = 'Jane Doe'
+  const phone2 = '44551234567'
+  const name3 = 'Foo bar'
+  const phone3 = '000'
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{name1} {phone1}</p>
+      <p>{name2} {phone2}</p>
+      <p>{name3} {phone3}</p>
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
-);
-
+)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
